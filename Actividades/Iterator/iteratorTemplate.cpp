50889b04e309e6a11e4cd99d66ada92726d3d29d
Horacio Rojas <horaciorojas737@hotmail.com>
2017-09-13 09:46:12 -0500

    Funcion find_if iterator

--- Actividades/Iterator/iteratorTemplate.cpp
+++ Actividades/Iterator/iteratorTemplate.cpp
@@ -28,6 +28,7 @@ public:
   }
 
   Iterator<T>* getIterator();
+
   T at(int pos)
   {
     return array[pos];
@@ -47,7 +48,6 @@ public:
       return true;
     return false;
   }
-
   T next()
   {
     return coll.at(cont++);
@@ -57,17 +57,36 @@ template <class T>
 Iterator<T> * Collection<T>::getIterator(){
   return new Iterator<T>(*this);
 }
+template<class T>
+bool par(T x)
+{
+  if(x % 2 == 0)
+    return true;
+  return false;
+}
+template<class T, typename Function>
+void find_if(Iterator<T> it, Function f)
+{
+  while (it.hasNext()){
+    T x = it.next();
+    if(f(x))
+      std::cout << x << " es par" <<'\n';
+  }
+}
 
 int main(int argc, char const *argv[]) {
-  Collection<char> i;
-  i.addElement('c');
-  i.addElement('a');
-  i.addElement('r');
-  i.addElement('r');
-  i.addElement('o');
-  Iterator<char>* it = i.getIterator();
+  Collection<int> i;
+  i.addElement(6);
+  i.addElement(5);
+  i.addElement(7);
+  i.addElement(8);
+  i.addElement(10);
+  Iterator<int>* it = i.getIterator();
   while (it->hasNext())
     std::cout << it->next() << '\n';
+  Iterator<int>* iterador = i.getIterator();
+  find_if<int, bool(*)(int)>(*iterador, par<int>);
   delete it;
+  delete iterador;
   return 0;
 }
